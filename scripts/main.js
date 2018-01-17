8808f754c17cdf87486ff939d2c7e2fe43516b0e
Sunny Wong <swong194@gmail.com>
2018-01-17 14:48:15 -0500

    adds audio on key down

--- scripts/main.js
+++ scripts/main.js
@@ -137,19 +137,85 @@ window.addEventListener('keydown', e => {
   let audio;
   switch (e.keyCode) {
     case 81:
-      audio = document.getElementById('a3');
+      audio = document.getElementById('1');
       break;
     case 87:
-      audio = document.getElementById('a5');
+      audio = document.getElementById('2');
       break;
     case 69:
-      audio = document.getElementById('c4');
+      audio = document.getElementById('3');
       break;
     case 82:
-      audio = document.getElementById('c5');
+      audio = document.getElementById('4');
+      break;
+    case 84:
+      audio = document.getElementById('5');
+      break;
+    case 89:
+      audio = document.getElementById('6');
+      break;
+    case 85:
+      audio = document.getElementById('7');
+      break;
+    case 73:
+      audio = document.getElementById('8');
+      break;
+    case 79:
+      audio = document.getElementById('9');
+      break;
+    case 80:
+      audio = document.getElementById('10');
+      break;
+    case 65:
+      audio = document.getElementById('11');
+      break;
+    case 83:
+      audio = document.getElementById('12');
+      break;
+    case 68:
+      audio = document.getElementById('13');
+      break;
+    case 70:
+      audio = document.getElementById('14');
+      break;
+    case 71:
+      audio = document.getElementById('15');
+      break;
+    case 74:
+      audio = document.getElementById('16');
+      break;
+    case 75:
+      audio = document.getElementById('17');
+      break;
+    case 76:
+      audio = document.getElementById('18');
+      break;
+    case 90:
+      audio = document.getElementById('19');
+      break;
+    case 88:
+      audio = document.getElementById('20');
+      break;
+    case 67:
+      audio = document.getElementById('21');
+      break;
+    case 66:
+      audio = document.getElementById('22');
+      break;
+    case 78:
+      audio = document.getElementById('23');
+      break;
+    case 77:
+      audio = document.getElementById('24');
+      break;
+    case 72:
+      audio = document.getElementById('25');
+      break;
+    case 86:
+      audio = document.getElementById('26');
       break;
     default:
-      audio = document.getElementById('a3');
+      audio = document.getElementById('27');
       break;
   }
   audio.currentTime = 0;
